189502ddf0123040739fed8ca68da52c679de9c5
xtrojan406 <moonton.re.ben@gmail.com>
2024-08-26 20:57:57 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,52 +1,59 @@
 (function() {
-    var _0x1c2a = ['users', 'admin', 'anonymous', 'password', 'expires', 'getItem', 'setItem', 'JSON', 'parse', 'stringify', 'value', 'getElementById', 'textContent', 'style', 'display', 'block', 'none', 'Akun ini telah kadaluarsa.', 'Password salah.', 'Username tidak ditemukan.', 'href', 'window', 'location', 'admin.html', 'main.html', 'Date', 'new', 'if', 'else', 'return', 'alert', 'function', 'loginError', 'currentDate', 'now', 'parseInt', 'console', 'log', 'alert', 'empty', 'split', 'join'];
+    var _0x2b4c = [
+        'users', 'admin', 'anonymous', 'password', 'expires', 'getItem', 'setItem',
+        'JSON', 'parse', 'stringify', 'value', 'getElementById', 'textContent',
+        'style', 'display', 'block', 'none', 'Akun ini telah kadaluarsa.',
+        'Password salah.', 'Username tidak ditemukan.', 'href', 'window',
+        'location', 'admin.html', 'main.html', 'Date', 'new', 'if', 'else',
+        'return'
+    ];
     
-    function _0x1b25(_0x2e9c, _0x3b18) {
-        return _0x1c2a[_0x2e9c];
+    function _0x1b1e(_0x1234, _0x5678) {
+        return _0x2b4c[_0x1234];
     }
     
-    function _0x12c0() {
-        if (!localStorage[_0x1b25(0)](_0x1b25(1))) {
-            localStorage[_0x1b25(2)](_0x1b25(1), JSON[_0x1b25(3)]({
-                [_0x1b25(4)]: { [_0x1b25(5)]: _0x1b25(6), [_0x1b25(7)]: null },
-                [_0x1b25(8)]: { [_0x1b25(5)]: _0x1b25(9), [_0x1b25(7)]: null }
+    function _0x3d3a() {
+        if (!localStorage[_0x1b1e(0)](_0x1b1e(1))) {
+            localStorage[_0x1b1e(2)](_0x1b1e(1), JSON[_0x1b1e(3)]({
+                [_0x1b1e(4)]: { [_0x1b1e(5)]: 'admin', [_0x1b1e(6)]: null },
+                [_0x1b1e(7)]: { [_0x1b1e(5)]: 'anonymous', [_0x1b1e(6)]: null }
             }));
         }
     }
 
-    function _0x1dc4() {
-        var _0x41d6 = document[_0x1b25(10)](_0x1b25(11))[_0x1b25(12)];
-        var _0x54e2 = document[_0x1b25(10)](_0x1b25(13))[_0x1b25(12)];
-        var _0x5d2a = document[_0x1b25(10)](_0x1b25(14));
+    function _0x5b7c() {
+        var _0x72bc = document[_0x1b1e(8)]('username')[_0x1b1e(9)];
+        var _0x8cfa = document[_0x1b1e(8)]('password')[_0x1b1e(9)];
+        var _0x4cda = document[_0x1b1e(8)]('loginError');
 
-        var _0x31f0 = JSON[_0x1b25(15)](localStorage[_0x1b25(0)](_0x1b25(1)));
+        var _0x4bfa = JSON[_0x1b1e(10)](localStorage[_0x1b1e(0)](_0x1b1e(1)));
 
-        if (_0x31f0[_0x41d6]) {
-            var _0x5b5d = _0x31f0[_0x41d6][_0x1b25(7)];
-            var _0x29c5 = new Date();
+        if (_0x4bfa[_0x72bc]) {
+            var _0x6d3e = _0x4bfa[_0x72bc][_0x1b1e(6)];
+            var _0x72b5 = new Date();
 
-            if (_0x5b5d && new Date(_0x5b5d) < _0x29c5) {
-                _0x5d2a[_0x1b25(12)] = _0x1b25(16);
-                _0x5d2a[_0x1b25(17)][_0x1b25(18)] = _0x1b25(19);
+            if (_0x6d3e && new Date(_0x6d3e) < _0x72b5) {
+                _0x4cda[_0x1b1e(9)] = _0x1b1e(11);
+                _0x4cda[_0x1b1e(12)].display = 'block';
                 return;
             }
 
-            if (_0x31f0[_0x41d6][_0x1b25(5)] === _0x54e2) {
-                if (_0x41d6 === _0x1b25(4)) {
-                    window[_0x1b25(20)][_0x1b25(21)] = _0x1b25(22);
+            if (_0x4bfa[_0x72bc][_0x1b1e(5)] === _0x8cfa) {
+                if (_0x72bc === _0x1b1e(4)) {
+                    window[_0x1b1e(13)][_0x1b1e(14)] = _0x1b1e(15);
                 } else {
-                    window[_0x1b25(20)][_0x1b25(21)] = _0x1b25(23);
+                    window[_0x1b1e(13)][_0x1b1e(14)] = _0x1b1e(16);
                 }
             } else {
-                _0x5d2a[_0x1b25(12)] = _0x1b25(24);
-                _0x5d2a[_0x1b25(17)][_0x1b25(18)] = _0x1b25(25);
+                _0x4cda[_0x1b1e(9)] = _0x1b1e(17);
+                _0x4cda[_0x1b1e(12)].display = 'block';
             }
         } else {
-            _0x5d2a[_0x1b25(12)] = _0x1b25(24);
-            _0x5d2a[_0x1b25(17)][_0x1b25(18)] = _0x1b25(26);
+            _0x4cda[_0x1b1e(9)] = _0x1b1e(17);
+            _0x4cda[_0x1b1e(12)].display = 'block';
         }
     }
 
-    _0x12c0();
-    window[_0x1b25(27)] = _0x1dc4;
+    _0x3d3a();
+    window['login'] = _0x5b7c;
 })();
