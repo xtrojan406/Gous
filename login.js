3a4a47fd8fdae6129394d0c5015ad407b3d25a4f
xtrojan406 <moonton.re.ben@gmail.com>
2024-08-26 21:04:23 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,59 +1,52 @@
 (function() {
-    var _0x2b4c = [
-        'users', 'admin', 'anonymous', 'password', 'expires', 'getItem', 'setItem',
-        'JSON', 'parse', 'stringify', 'value', 'getElementById', 'textContent',
-        'style', 'display', 'block', 'none', 'Akun ini telah kadaluarsa.',
-        'Password salah.', 'Username tidak ditemukan.', 'href', 'window',
-        'location', 'admin.html', 'main.html', 'Date', 'new', 'if', 'else',
-        'return'
+    var _0x2e8b = [
+        'users', 'getItem', 'setItem', 'stringify', 'parse', 'admin', 'anonymous',
+        'password', 'expires', 'value', 'getElementById', 'textContent', 'style',
+        'display', 'block', 'none', 'Akun ini telah kadaluarsa.', 'Password salah.',
+        'Username tidak ditemukan.', 'Date', 'now', 'href', 'location', 'admin.html',
+        'main.html'
     ];
-    
-    function _0x1b1e(_0x1234, _0x5678) {
-        return _0x2b4c[_0x1234];
-    }
-    
-    function _0x3d3a() {
-        if (!localStorage[_0x1b1e(0)](_0x1b1e(1))) {
-            localStorage[_0x1b1e(2)](_0x1b1e(1), JSON[_0x1b1e(3)]({
-                [_0x1b1e(4)]: { [_0x1b1e(5)]: 'admin', [_0x1b1e(6)]: null },
-                [_0x1b1e(7)]: { [_0x1b1e(5)]: 'anonymous', [_0x1b1e(6)]: null }
-            }));
-        }
+
+    function _0x1a2c(_0x23b9, _0x1a2b) {
+        return _0x2e8b[_0x23b9];
     }
 
-    function _0x5b7c() {
-        var _0x72bc = document[_0x1b1e(8)]('username')[_0x1b1e(9)];
-        var _0x8cfa = document[_0x1b1e(8)]('password')[_0x1b1e(9)];
-        var _0x4cda = document[_0x1b1e(8)]('loginError');
+    if (!localStorage[_0x1a2c(1)](_0x1a2c(0))) {
+        localStorage[_0x1a2c(2)](_0x1a2c(0), JSON[_0x1a2c(3)]({
+            [_0x1a2c(4)]: { [_0x1a2c(5)]: 'admin', [_0x1a2c(6)]: null },
+            [_0x1a2c(7)]: { [_0x1a2c(5)]: 'anonymous', [_0x1a2c(6)]: null }
+        }));
+    }
 
-        var _0x4bfa = JSON[_0x1b1e(10)](localStorage[_0x1b1e(0)](_0x1b1e(1)));
+    function _0x1a2c() {
+        var _0x3b29 = document[_0x1a2c(8)]('username')[_0x1a2c(9)];
+        var _0x2f7e = document[_0x1a2c(8)]('password')[_0x1a2c(9)];
+        var _0x5c76 = document[_0x1a2c(8)]('loginError');
+        
+        var _0x5a8b = JSON[_0x1a2c(4)](localStorage[_0x1a2c(1)](_0x1a2c(0)));
+        var _0x3d9e = _0x5a8b[_0x3b29];
+        
+        if (_0x3d9e) {
+            var _0x3e9d = _0x3d9e[_0x1a2c(6)];
+            var _0x5b0d = new Date();
 
-        if (_0x4bfa[_0x72bc]) {
-            var _0x6d3e = _0x4bfa[_0x72bc][_0x1b1e(6)];
-            var _0x72b5 = new Date();
-
-            if (_0x6d3e && new Date(_0x6d3e) < _0x72b5) {
-                _0x4cda[_0x1b1e(9)] = _0x1b1e(11);
-                _0x4cda[_0x1b1e(12)].display = 'block';
+            if (_0x3e9d && new Date(_0x3e9d) < _0x5b0d) {
+                _0x5c76[_0x1a2c(9)] = _0x1a2c(10);
+                _0x5c76[_0x1a2c(11)].display = _0x1a2c(12);
                 return;
             }
 
-            if (_0x4bfa[_0x72bc][_0x1b1e(5)] === _0x8cfa) {
-                if (_0x72bc === _0x1b1e(4)) {
-                    window[_0x1b1e(13)][_0x1b1e(14)] = _0x1b1e(15);
-                } else {
-                    window[_0x1b1e(13)][_0x1b1e(14)] = _0x1b1e(16);
-                }
+            if (_0x3d9e[_0x1a2c(5)] === _0x2f7e) {
+                window[_0x1a2c(13)][_0x1a2c(14)] = _0x1a2c(15);
             } else {
-                _0x4cda[_0x1b1e(9)] = _0x1b1e(17);
-                _0x4cda[_0x1b1e(12)].display = 'block';
+                _0x5c76[_0x1a2c(9)] = _0x1a2c(16);
+                _0x5c76[_0x1a2c(11)].display = _0x1a2c(12);
             }
         } else {
-            _0x4cda[_0x1b1e(9)] = _0x1b1e(17);
-            _0x4cda[_0x1b1e(12)].display = 'block';
+            _0x5c76[_0x1a2c(9)] = _0x1a2c(16);
+            _0x5c76[_0x1a2c(11)].display = _0x1a2c(12);
         }
     }
 
-    _0x3d3a();
-    window['login'] = _0x5b7c;
+    window['login'] = _0x1a2c;
 })();
